62b1388e1885eccc6e7cfd53aa5a9fb355284af3
xxai.art <xxai.art@gmail.com>
2023-06-01 03:37:59 +0800

    ♨

--- proc.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-from flagai.model.mm.AltCLIP import AltCLIPProcess
-from config import MODEL_FP
-
-proc = AltCLIPProcess.from_pretrained(MODEL_FP)
-
-tokenizer = proc.tokenizer
-transform = proc.feature_extractor
