2252aa833fe44afd136a871b871fce08cdfa1d76
xxai.art <xxai.art@gmail.com>
2023-06-01 00:38:05 +0800

    ♨

--- clip_model.py
+++ clip_model.py
@@ -2,7 +2,7 @@
 
 import torch
 import torch.nn as nn
-from config import DIR_MODEL, FP_MODEL
+from config import DIR_MODEL, NAME_MODEL
 from flagai.auto_model.auto_loader import AutoLoader
 from proc import tokenizer
 
@@ -16,7 +16,7 @@ else:
 DEVICE = torch.device(device)
 
 loader = AutoLoader(task_name="txt_img_matching",
-                    model_name=FP_MODEL,
+                    model_name=NAME_MODEL,
                     model_dir=DIR_MODEL)
 
 MODEL = loader.get_model()
