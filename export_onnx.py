1f726dc6ff30c883591f4410227fcafa39ffff1f
xxai.art <xxai.art@gmail.com>
2023-06-01 01:23:36 +0800

    ♨

--- export_onnx.py
+++ export_onnx.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 
+from os import makedirs, rename
 from os.path import join
 import torch
-from config import ONNX_FP, ROOT, DEVICE, opset_version
+from config import ONNX_FP, ONNX_DIR, MODEL_NAME, ROOT, DEVICE, opset_version
 from clip_model import IMG
 from PIL import Image
 from proc import transform
@@ -13,11 +14,20 @@ image = Image.open(JPG)
 image = transform(image)
 image = torch.tensor(image["pixel_values"]).to(DEVICE)
 
-torch.onnx.export(
+
+def onnx_export(outdir, model, args, **kwds):
+  onnx_txt = join(ONNX_FP, outdir)
+  makedirs(onnx_txt, exist_ok=True)
+  name = f'{MODEL_NAME}.{outdir}.onnx'
+  fp = join(onnx_txt, name)
+  torch.onnx.export(model, args, fp, **kwds)
+  rename(fp, join(ONNX_DIR, name))
+
+
+onnx_export(
+    'img',
     IMG,  # model being run
     image,  # model input (or a tuple for multiple inputs)
-    ONNX_FP +
-    "img.onnx",  # where to save the model (can be a file or file-like object)
     export_params=
     True,  # store the trained parameter weights inside the model file
     opset_version=opset_version,  # the ONNX version to export the model to
@@ -28,7 +38,6 @@ torch.onnx.export(
     dynamic_axes={'input': {
         0: 'batch'
     }})
-
 # torch.onnx.export(
 #     TXT,  # model being run
 #     text,  # model input (or a tuple for multiple inputs)
