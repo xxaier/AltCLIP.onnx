eea53755df618041fde18f2097645b6a54c1f468
xxai.art <xxai.art@gmail.com>
2023-06-01 01:35:26 +0800

    ♨

--- onnx_check.py
+++ onnx_check.py
@@ -4,5 +4,5 @@ from config import ONNX_FP, MODEL_NAME
 from os.path import join
 import onnx
 
-onnx_model = onnx.load(join(ONNX_FP, f'img/{MODEL_NAME}.img.onnx'))
-onnx.checker.check_model(onnx_model)
+fp = join(ONNX_FP, f'img/{MODEL_NAME}.img.onnx')
+onnx.checker.check_model(fp)
