87b847528d02c22bd6e0c0b1cc46225d67d6b942
xxai.art <xxai.art@gmail.com>
2023-06-01 02:02:22 +0800

    ♨

--- onnx_check.py
+++ onnx_check.py
@@ -6,10 +6,11 @@ import onnx
 
 
 def check(name):
-  fp = join(ONNX_FP, f'{name}/{MODEL_NAME}.{name}.onnx')
+  fp = join(ONNX_FP, f'{MODEL_NAME}.{name}.onnx')
   print(fp, onnx.checker.check_model(fp))
   # onnx_model = onnx.load(fp)
   # return onnx_model
 
 
 check('img')
+check('txt')
