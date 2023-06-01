8b07b33d2b450753965b69158c2ab26456b3994a
xxai.art <xxai.art@gmail.com>
2023-06-01 12:34:36 +0800

    ♨

--- onnx_img.py
+++ onnx_img.py
@@ -10,15 +10,15 @@ class ImgVec:
   def __init__(self):
     self.sess = onnx_load('img')
 
-  def __call__(self, image):
-    output = self.sess.run(None, {'input': transform(image).numpy()})
+  def __call__(self, img):
+    output = self.sess.run(None, {'input': transform(img)})
     return output
 
 
 if __name__ == '__main__':
-  from wrap.config import ROOT
+  from wrap.config import IMG_DIR
   from os.path import join
-  img = Image.open(join(ROOT, 'img/cat.jpg'))
+  img = Image.open(join(IMG_DIR, 'cat.jpg'))
   img2vec = ImgVec()
   vec = img2vec(img)
   print(vec)
