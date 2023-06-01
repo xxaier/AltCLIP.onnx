3d86f1ac904a2cde108d18b71f63fcd0fae5390a
xxai.art <xxai.art@gmail.com>
2023-06-01 11:48:04 +0800

    ♨

--- clip_txt.py
+++ clip_txt.py
@@ -1,53 +1,24 @@
 #!/usr/bin/env python
 
 import torch
-from PIL import Image
 from os.path import join
-from time import time
 from glob import glob
 from wrap.config import ROOT
-from wrap.proc import transform, tokenizer
-from wrap.device import DEVICE
-from wrap.clip_model import IMG, TXT
-
-COST = None
+from wrap.proc import tokenizer
+from wrap.clip_model import TXT
 
 
-def inference(jpg, tmpl_kind_li):
-  global COST
-  image = Image.open(jpg)
-  image = transform(image)
-  image = torch.tensor(image["pixel_values"]).to(DEVICE)
-  print('image.size', image.size())
+def txt2vec(li):
   with torch.no_grad():
-    begin = time()
-    image_features = IMG.forward(image)
-    if COST is not None:
-      COST += (time() - begin)
-
-  for tmpl, kind_li in tmpl_kind_li:
-    begin = time()
-    with torch.no_grad():
-      li = []
-      for i in kind_li:
-        li.append(tmpl % i)
-      text_features = TXT.forward(*tokenizer(li))
-      text_probs = (image_features @ text_features.T).softmax(dim=-1)
-
-    if COST is not None:
-      COST += (time() - begin)
-
-      for kind, p in zip(kind_li, text_probs.cpu().numpy()[0].tolist()):
-        p = round(p * 10000)
-        if p:
-          print("  %s %.1f%%" % (kind, p / 100))
-  return
+    return TXT.forward(*tokenizer(li))
 
 
 if __name__ == "__main__":
   li = glob(join(ROOT, 'jpg/*.jpg'))
-  # 预热，py.compile 要第一次运行才编译
-  txt2vec((
-      'a photo of dog',
-      'a photo for chinese woman',
-  ))
+  from test_txt import TEST_TXT
+  for li in TEST_TXT:
+    r = txt2vec(li)
+    for txt, i in zip(li, r):
+      print(txt)
+      print(i)
+      print('\n')
